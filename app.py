e45c78b3ce9e33e90358f8c8d29407182d0dfba9
anson950705888-star <anson950705888@gmail.com>
2025-11-13 11:39:34 +0800

    Update author names in blog posts

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ posts = [
     {
         "id": 1,
         "title": "第一篇文章",
-        "author": "柏諺",
+        "author": "李柏諺",
         "content": "這是我的第一篇 Flask 部落格文章！"
     },
     {
         "id": 2,
         "title": "學習 Flask 的心得",
-        "author": "小明",
+        "author": "李柏諺",
         "content": "Flask 的模板系統真的很方便，Jinja2 可以讓我們輕鬆地插入變數。"
     }
 ]
